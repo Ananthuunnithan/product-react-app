4006d86dfbb1011c537e190f0fe226d06d087c8c
Ananthu Unnithan <unnithanananthu69@gmail.com>
2026-07-10 11:02:53 +0530

    dynamic created

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const ViewAll = () => {
+
+const [data,changeData] = useState(
+    [
+        {"model":"Iphone 17","avatar":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZuaA6RNqgOCu14Bl7H0OtHimQFNLho9-X8rLappcM0g&s=10","description":"Latest on Amazon"},
+        {"model":"Motorola Edge 60 series","avatar":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPx8hP9hWy7zRvurxHy71FORHv-FfrLZ6SXS2NnZeEOw&s","description":"Latest on Amazon"},
+        {"model":"Nothing 2a","avatar":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrJb_8jLOuk7uo0iLMzg3i2um1K-UwJ8GX8Hr9Wh8uag&s=10","description":"Latest on Amazon"},
+        {"model":"One Plus Nord 6","avatar":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVOCSYXRUNG39xeoTV-ANfHxxUmTuzGALbwngCu9OLEQ&s=10","description":"Latest on Amazon"},
+        {"model":"Samsung Galaxy 25","avatar":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5xlBZQLrC6KjPS6R7_-KVi1Il4ivbiWmd4dRx9H_JTg&s=10","description":"Latest on Amazon"},
+
+    ]
+)
+
   return (
     <div>
 <NavBar/>
@@ -10,126 +22,26 @@ const ViewAll = () => {
             <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                 <div class="row g-4">
-                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                    {data.map(
+                        (value,index) => {
+                            return(
+                                <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                         <div class="card">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZuaA6RNqgOCu14Bl7H0OtHimQFNLho9-X8rLappcM0g&s=10" class="card-img-top" alt="..." height="350px"/>
+                            <img src={value.avatar} class="card-img-top" alt="..." height="350px"/>
                             <div class="card-body">
-                                <h5 class="card-title">Iphone 17</h5>
-                                <p class="  card-text">Latest on amazon</p>
+                                <h5 class="card-title">{value.model}</h5>
+                                <p class="  card-text">{value.description}</p>
                                 <a href="#" class="btn btn-primary">Buy now</a>
                             </div>
                         </div>
 
                     </div>
-                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            )
+                        }
+                    )}
+                    
 
-                        <div class="card">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPx8hP9hWy7zRvurxHy71FORHv-FfrLZ6SXS2NnZeEOw&s" class="card-img-top" alt="..." height="350px"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Motrola Edge 60 series</h5>
-                                <p class="card-text">Latest on amazon</p>
-                                <a href="#" class="btn btn-primary">Buy now</a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                        <div class="card">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrJb_8jLOuk7uo0iLMzg3i2um1K-UwJ8GX8Hr9Wh8uag&s=10" class="card-img-top" alt="..." height="350px"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Nothing 2a</h5>
-                                <p class="card-text">Latest on amazon</p>
-                                <a href="#" class="btn btn-primary">Buy now</a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                        <div class="card">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5xlBZQLrC6KjPS6R7_-KVi1Il4ivbiWmd4dRx9H_JTg&s=10" class="card-img-top" alt="..." height="350px"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Samsung galaxy 25</h5>
-                                <p class="card-text">Latest on amazon</p>
-                                <a href="#" class="btn btn-primary">Buy now</a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                        <div class="card">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVOCSYXRUNG39xeoTV-ANfHxxUmTuzGALbwngCu9OLEQ&s=10" class="card-img-top" alt="..." height="350px"/>
-                            <div class="card-body">
-                                <h5 class="card-title">One plus node6</h5>
-                                <p class="card-text">Latest on amazon</p>
-                                <a href="#" class="btn btn-primary">Buy now</a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                        <div class="card">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrXfdHPVLzXDkjrU-ZRIyLqn4w1KsXAkEngKaY91okUw&s=10" class="card-img-top" alt="..." height="350px"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Xiaomi 15T</h5>
-                                <p class="card-text">Latest on amazon</p>
-                                <a href="#" class="btn btn-primary">Buy now</a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                        <div class="card">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ5Z1C-Ii9v8igbewsI2A19PMzjlGwP5P6F4JsG3252A&s=10" class="card-img-top" alt="..." height="350px"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Google Pixel 10</h5>
-                                <p class="card-text">Latest on amazon</p>
-                                <a href="#" class="btn btn-primary">Buy now</a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                        <div class="card">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa3wC7M5l1jcyw_Q37hQoDD9BX63mzDp5hpqY1VIT_ig&s=10" class="card-img-top" alt="..." height="350px"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Samsung Z fold</h5>
-                                <p class="card-text">Latest on amazon</p>
-                                <a href="#" class="btn btn-primary">Buy now</a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                        <div class="card">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF3gtZ95hU3w6H2pUlU9n_uYtcgcAtdocvSB_9BADAcw&s=10" class="card-img-top" alt="..." height="350px"/>
-                            <div class="card-body">
-                                <h5 class="card-title">redmi note 15</h5>
-                                <p class="card-text">Latest on amazon</p>
-                                <a href="#" class="btn btn-primary">Buy now</a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                        <div class="card">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4D7_tbTRGiCpzvidsA-DMuirymCLC-hJP9rFs4mvCRg&s" class="card-img-top" alt="..." height="350px"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Motorola Edge fusion70</h5>
-                                <p class="card-text">Latest on amazon</p>
-                                <a href="#" class="btn btn-primary">Buy now</a>
-                            </div>
-                        </div>
-
-                    </div>
                 </div>
 
             </div>
